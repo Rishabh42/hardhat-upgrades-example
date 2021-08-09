80fa078776fa59a82e07e6a1f930b09047273d22
Rishabh Thaney <rishabhthaney@gmail.com>
2021-08-09 14:57:12 +0530

    upgrade script working on kovan

--- scripts/prepare_upgrade.js
+++ scripts/prepare_upgrade.js
@@ -1,10 +1,18 @@
 // scripts/prepare_upgrade.js
 async function main() {
-  const proxyAddress = "0x9259C08BBC99c54477b9E0f3EC5176848d65D94c";
+  const proxyAddress = "0x23FbcaeD67886DCB230A7B428861Bc96BeD42921";
+  const safeMathLib = await ethers.getContractFactory("SafeMath");
+  const _safeMathLib = await safeMathLib.deploy();
 
-  const BoxV2 = await ethers.getContractFactory("BoxV2");
+  BoxV2 = await ethers.getContractFactory("BoxV2", {
+    libraries: {
+      SafeMath: _safeMathLib.address,
+    },
+  });
   console.log("Preparing upgrade...");
-  const boxV2Address = await upgrades.prepareUpgrade(proxyAddress, BoxV2);
+  const boxV2Address = await upgrades.prepareUpgrade(proxyAddress, BoxV2, {
+    unsafeAllow: ["external-library-linking"],
+  });
   console.log("BoxV2 at:", boxV2Address);
 }
 
